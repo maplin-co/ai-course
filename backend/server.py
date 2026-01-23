42717035ea8fb30645df59cf2c1c98bc087fb1b6
rasali535 <maplininc@gmail.com>
2026-01-23 16:38:25 +0200

    docker buildx build

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from starlette.middleware.cors import CORSMiddleware
-import os
 import logging
+from fastapi.staticfiles import StaticFiles
 from backend.database import close_mongo_connection
 from backend.routers import auth, resources, payments
 
@@ -30,6 +30,14 @@ api_router.include_router(resources.router, prefix="/resources", tags=["resource
 api_router.include_router(payments.router, prefix="/payments", tags=["payments"])
 
 app.include_router(api_router)
+
+# Mount static files (at the end so API takes precedence)
+static_path = os.path.join(os.path.dirname(__file__), "static")
+if os.path.exists(static_path):
+    app.mount("/", StaticFiles(directory=static_path, html=True), name="static")
+elif os.path.exists("static"):
+    app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
 
 # Events
 @app.on_event("shutdown")
