824ae2826364782c6b8aafe893c37907b8ffa2cc
rasali535 <maplininc@gmail.com>
2026-01-27 18:25:14 +0200

    Add Docker auto-publish workflow

--- backend/server.py
+++ backend/server.py
@@ -16,7 +16,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000,http://localhost:8080').split(','),
+    allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000,http://localhost:8080,https://pohei.de,https://www.pohei.de').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
